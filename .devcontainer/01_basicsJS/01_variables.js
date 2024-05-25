9da05cfb5f94e0d62f23eaec8bc12557424a09f6
AdityaKumaar21 <113281443+AdityaKumaar21@users.noreply.github.com>
2024-05-25 15:33:29 +0000

    datatypes and ecma

---
+++ .devcontainer/01_basicsJS/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=12548
+let accountName="Aditya Chaurasia"
+
+var AccountName="Abhi"
+var AccountName="abhii"
+let accoutState;
+console.table([accountId,accountName,AccountName,accoutState])
+console.log(typeof null)
+console.log(typeof undefined);
