d31bc6cd3be68caf8e27117e56bb63d38c0762ba
AdityaKumaar21 <113281443+AdityaKumaar21@users.noreply.github.com>
2024-05-26 18:28:17 +0000

    full arrays

--- .devcontainer/01_basicsJS/arr2.js
+++ .devcontainer/01_basicsJS/arr2.js
@@ -5,3 +5,10 @@ let arr3=new Array(123,145.555)
 // const newArr=arr1.concat(arr2)  //concat usage
 let newArr=[...arr1,...arr2,...arr3]   //spread opertor ...
 console.log(newArr);
+let a=[1,2,4,[1,3,5],3,[2,4,[5,2,1]]]
+// console.log(a.flat(Infinity));
+let q=1
+let w=2
+let e=3
+console.log(Array.of(q,w,e));
+Array.
